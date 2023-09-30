2b983bff3629ab39a6a58f2c7b9735a5311f681c
oleg liudovskyi <olegsvit@gmail.com>
2023-09-30 11:14:53 -0400

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // API à utiliser pour le travail: https://randomuser.me/api/?results=5
+//TP#2
+
+//fetch('https://randomuser.me/api/?results=5')
+//.then (response => response.json())
+//.then (data => console.log(data))
+//.catch(error => console.log(error));
+
+
+
+fetch('https://randomuser.me/api/?results=5')
+.then (response => response.json())
+.then (data => {
+    let output = '<h2>List of users</h2>';
+    data.forEach(function(element) {
+        output+=element.gender + ' - ' 
+        output+=element.name + ' - '
+        output+=element.location + ' - '
+        output+=element.email + ' - '
+        output+=element.picture + '<br/>'  
+    });
+    console.log(output)
+    document.getElementById('user').innerHTML = output
+    console.log(data);
+});
