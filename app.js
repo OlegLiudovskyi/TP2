14f0ca7df68211eaae61779ee184168dd0978349
Oleg Liudovslyi <olegsvit@gmail.com>
2023-09-30 14:09:49 -0400

    Finish 30_09_2023

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 // API à utiliser pour le travail: https://randomuser.me/api/?results=5
 //TP#2
 
-//fetch('https://randomuser.me/api/?results=5')
-//.then (response => response.json())
-//.then (data => console.log(data))
-//.catch(error => console.log(error));
-
-
-
 fetch('https://randomuser.me/api/?results=5')
 .then (response => response.json())
 .then (data => {
     let output = '<h2>List of users</h2>';
-    data.forEach(function(element) {
-        output+=element.gender + ' - ' 
-        output+=element.name + ' - '
-        output+=element.location + ' - '
-        output+=element.email + ' - '
-        output+=element.picture + '<br/>'  
-    });
+    let rootEl = document.querySelector("main");
+
+    let html = "";
+
+    for(let post of data.results) {
+        html += `
+        <div class="post">
+            <div class="mb-10"><img src="${post.picture.large}" alt="picture"></div>
+            <div class="mb-10">Nom: ${post.name.first} ${post.name.last}</div>
+            <div class="mb-10">E-mail: ${post.email}</div>
+        </div>       
+        `
+    }
+
+    rootEl.innerHTML = html
     console.log(output)
-    document.getElementById('user').innerHTML = output
-    console.log(data);
+    // document.getElementById('user').innerHTML = output
+    console.log(data.results);
 });
